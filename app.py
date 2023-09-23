0741dbdebd096ac173e3782eb271c0eb281092e8
abdollah <32410133+abdollahchelasi@users.noreply.github.com>
2023-09-23 14:26:37 +0330

    Create app.py

---
+++ app.py
@@ -0,0 +1,9 @@
+import telebot
+
+telebot.TeleBot('6590512531:AAHjC6A270nF9WCdntFhrTUuW5aJbbcVXus')
+
+@bot.message_handler(commands=['start'])
+def start(message):
+    bot.send_message(message.chat.id, 'سلام!')
+
+bot.polling()
